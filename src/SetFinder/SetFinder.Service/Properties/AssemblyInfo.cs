bd9d4a30fdf0d905e70d068606554bb2d6312fff
chris-e-watson <chris@watson.me.uk>
2018-06-06 18:47:59 +0100

    Initial solution structure.

---
+++ src/SetFinder/SetFinder.Service/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Note: Shared assembly attributes can be found in SharedAssemblyInfo.cs.
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SetFinder.Service")]
+[assembly: AssemblyDescription("SetFinder.Service")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("49c21412-fd4a-464f-aa47-9456dde3741f")]
